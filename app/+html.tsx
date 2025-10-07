8590b0a5e3559d13db83664cb7f75328fa7e9054
sierrabonita <workbj1212@gmail.com>
2025-10-07 12:35:37 +0900

    lint and format

--- app/+html.tsx
+++ app/+html.tsx
@@ -1,4 +1,4 @@
-import { ScrollViewStyleReset } from 'expo-router/html';
+import { ScrollViewStyleReset } from "expo-router/html";
 
 // This file is web-only and used to configure the root HTML for every
 // web page during static rendering.
@@ -19,7 +19,8 @@ export default function Root({ children }: { children: React.ReactNode }) {
         <ScrollViewStyleReset />
 
         {/* Using raw CSS styles as an escape-hatch to ensure the background color never flickers in dark-mode. */}
-        <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
+        {/* <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} /> */}
+        <style>{responsiveBackground}</style>
         {/* Add any additional <head> elements that you want globally available on web... */}
       </head>
       <body>{children}</body>
